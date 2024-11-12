7d66b8eed662b4d5cfcf8a51c0a7e0fb722907bf
Vaibhav Goyal <vaibhavgoyal2310066@gmail.com>
2024-11-12 22:17:38 +0530

    variables

---
+++ 01_variable.js
@@ -0,0 +1,8 @@
+const account_Id = 14453
+let accountemai="vaibhav.com"
+var accpass="12345"
+accountCity="jaipur"
+
+console.log(account_Id)
+console.table([account_Id,accountemai,accpass,accountCity])
+
