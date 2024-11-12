f66ee07943ed7192148512e03d94ccdd838560a0
Vaibhav Goyal <vaibhavgoyal2310066@gmail.com>
2024-11-12 22:26:12 +0530

    Datatypes

---
+++ 02_Datatypes.js
@@ -0,0 +1,36 @@
+"use strict"; // treat all Js code as newer version
+
+let name = "John";
+let age = 25;
+let isApproved = false;
+// range of number in JS is -2^53 to 2^53
+// bigint is used to store large number
+// string => ""
+// boolean => true/false
+// null is stand alone value
+
+// difference between null and undefined is null is explicitly assigned by developer
+// and undefined is default value
+
+// symbol is used to create unique value
+
+// console.log(typeof null) // object;
+// console.log(typeof undefined) // undefined
+// console.log(typeof name) // string
+// console.log(typeof age) // number
+// console.log(typeof isApproved) // boolean
+// console.log(typeof 1) // number
+// console.log(typeof 1.1) // number
+// console.log(typeof NaN) // number
+// console.log(typeof Infinity) // number
+// console.log(typeof Symbol()) // symbol
+// console.log(typeof {}) // object
+// console.log(typeof []) // object
+// console.log(typeof function() {}) // function
+// console.log(typeof new Date()) // object
+// console.log(typeof /a/) // object
+// console.log(typeof new Map()) // object
+// console.log(typeof new Set()) // object
+// console.log(typeof new WeakMap()) // object
+// console.log(typeof new WeakSet()) // object
+// console.log(typeof new Promise()) // object
