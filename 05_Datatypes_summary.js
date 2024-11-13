22042e1742385dbfb33d2ebe94c887ff50100e75
Vaibhav Goyal <vaibhavgoyal2310066@gmail.com>
2024-11-13 23:07:49 +0530

    Datatypes

---
+++ 05_Datatypes_summary.js
@@ -0,0 +1,18 @@
+// Primitive
+// 7 types of primitive datatypes
+// 1. number
+// 2. string
+// 3. boolean
+// 4. undefined
+// 5. null
+// 6. symbol
+// 7. bigint
+
+// Reference type or Non-primitive
+// 1. Object
+// 2. Array
+// 3. Function
+
+// Js is dynamically typed language
+
+consosle.log(typeof 1); // number
